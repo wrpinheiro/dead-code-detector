a287828164ca0e250a36c16afe669a96da750633
Wellington Ricardo Pinheiro <wellington.pinheiro@gmail.com>
2017-03-26 10:11:15 -0300

    refactor: update build script to use the version in build.gradle file

--- build_and_deploy.sh
+++ build_and_deploy.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
-#./gradlew build
+export APP_VERSION=$(grep "project\.version" build.gradle | sed -E "s/(.*) = '(.*)'/\2/")
+
+./gradlew build
 #export DOCKER_HOST="tcp://build.swarm.devfactory.com"
 docker build -t wpinheiro_dead_code_detection_service .
-docker tag  wpinheiro_dead_code_detection_service registry2.swarm.devfactory.com/devfactory/wpinheiro_dead_code_detection_service
-docker push registry2.swarm.devfactory.com/devfactory/wpinheiro_dead_code_detection_service
+docker tag  wpinheiro_dead_code_detection_service registry2.swarm.devfactory.com/devfactory/wpinheiro_dead_code_detection_service:$APP_VERSION
+docker push registry2.swarm.devfactory.com/devfactory/wpinheiro_dead_code_detection_service:$APP_VERSION
 
 export DOCKER_HOST="tcp://webserver.devfactory.com"
 docker service ps wpinheiro_dead_code_detection_service
 docker service rm wpinheiro_dead_code_detection_service
-docker service create -p 17951:8080 --name wpinheiro_dead_code_detection_service --replicas 1 --limit-memory 1g registry2.swarm.devfactory.com/devfactory/wpinheiro_dead_code_detection_service
+docker service create -p 17951:8080 --name wpinheiro_dead_code_detection_service --replicas 1 --limit-memory 1g registry2.swarm.devfactory.com/devfactory/wpinheiro_dead_code_detection_service:$APP_VERSION
 docker service ps wpinheiro_dead_code_detection_service
