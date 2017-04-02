093350c408746d08899fa5b18694f5d7286001c2
Wellington Ricardo Pinheiro <wellington.pinheiro@gmail.com>
2017-04-01 21:15:16 -0300

    fix: update und timeout to 240

--- build_and_deploy.sh
+++ build_and_deploy.sh
@@ -17,6 +17,12 @@ docker push registry2.swarm.devfactory.com/devfactory/wpinheiro_dead_code_detect
 
 docker -H tcp://webserver.devfactory.com service ps wpinheiro_dead_code_detection_service
 docker -H tcp://webserver.devfactory.com service rm wpinheiro_dead_code_detection_service
-docker -H tcp://webserver.devfactory.com service create -p 17951:8080 --name wpinheiro_dead_code_detection_service --replicas 1 --limit-memory 1g registry2.swarm.devfactory.com/devfactory/wpinheiro_dead_code_detection_service:$APP_VERSION
+#docker -H tcp://webserver.devfactory.com service create -p 17951:8080 --name wpinheiro_dead_code_detection_service --replicas 1 --limit-memory 1g registry2.swarm.devfactory.com/devfactory/wpinheiro_dead_code_detection_service:$APP_VERSION
+docker -H tcp://webserver.devfactory.com service create -p 17951:8080 --name wpinheiro_dead_code_detection_service --replicas 1 --limit-memory 1g -e UND_TIMEOUT=240 registry2.swarm.devfactory.com/devfactory/wpinheiro_dead_code_detection_service:$APP_VERSION
 docker -H tcp://webserver.devfactory.com service ps wpinheiro_dead_code_detection_service
 
+
+docker -H tcp://webserver.devfactory.com node ls
+docker -H 10.224.103.48 ps | grep wpinheiro
+
+docker -H 10.224.103.48 exec -it 1c7781a18d9a tail -f /var/log/deadcodedetection/server.log
